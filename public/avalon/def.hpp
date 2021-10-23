ffdb94f90b0707864c5500b5eeff2cda84aae412
bachelorwang <whc@bachelor-studio.com>
2021-10-23 13:39:46 +0800

    add type alias, add field of team build traits

--- public/avalon/def.hpp
+++ public/avalon/def.hpp
@@ -4,7 +4,15 @@
 namespace avalon {
 
 using player_count_t = int32_t;
-using round_index_t = int32_t;
-constexpr int32_t round_count = 5;
+using player_index_t = player_count_t;
+using round_count_t = int32_t;
+using round_index_t = round_count_t;
+using player_id_t = uint64_t;
+
+constexpr round_count_t round_count = 5;
+constexpr player_count_t min_player_count = 7;
+constexpr player_count_t max_player_count = 10;
+constexpr int32_t max_voting_count = 5;
+constexpr round_count_t min_quest_count = 3;
 
 }  // namespace avalon
--- public/avalon/game/team_build.hpp
+++ public/avalon/game/team_build.hpp
@@ -8,32 +8,38 @@ struct team_build_requirement;
 
 template <>
 struct team_build_requirement<5> {
-  static constexpr int32_t value[round_count] = {2, 3, 2, 3, 3};
+  static constexpr player_count_t max_size = 3;
+  static constexpr player_count_t value[round_count] = {2, 3, 2, 3, 3};
 };
 
 template <>
 struct team_build_requirement<6> {
-  static constexpr int32_t value[round_count] = {2, 3, 4, 3, 4};
+  static constexpr player_count_t max_size = 4;
+  static constexpr player_count_t value[round_count] = {2, 3, 4, 3, 4};
 };
 
 template <>
 struct team_build_requirement<7> {
-  static constexpr int32_t value[round_count] = {2, 3, 3, 4, 4};
+  static constexpr player_count_t max_size = 4;
+  static constexpr player_count_t value[round_count] = {2, 3, 3, 4, 4};
 };
 
 template <>
 struct team_build_requirement<8> {
-  static constexpr int32_t value[round_count] = {3, 4, 4, 5, 5};
+  static constexpr player_count_t max_size = 5;
+  static constexpr player_count_t value[round_count] = {3, 4, 4, 5, 5};
 };
 
 template <>
 struct team_build_requirement<9> {
-  static constexpr int32_t value[round_count] = {3, 4, 4, 5, 5};
+  static constexpr player_count_t max_size = 5;
+  static constexpr player_count_t value[round_count] = {3, 4, 4, 5, 5};
 };
 
 template <>
 struct team_build_requirement<10> {
-  static constexpr int32_t value[round_count] = {3, 4, 4, 5, 5};
+  static constexpr player_count_t max_size = 5;
+  static constexpr player_count_t value[round_count] = {3, 4, 4, 5, 5};
 };
 
 }  // namespace avalon
